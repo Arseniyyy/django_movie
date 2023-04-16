29f6e191c2c43f7fc3c7fde83a3970cc41fb009a
arseniyyy <arskosh05@mail.ru>
2023-04-16 22:40:23 +0700

    Add email settings for the smtp protocol that sends messages from outlook.com.

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -54,14 +54,32 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        "rest_framework.authentication.TokenAuthentication",
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.office365.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+SITE_NAME = "movie-finder"
+
 # Djoser settings
 DJOSER = {
+    'SEND_ACTIVATION_EMAIL': True,
+    # "USER_ID_FIELD": "pk",
+    "LOGIN_FIELD": "email",
     'USER_CREATE_PASSWORD_RETYPE': True,
+    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'PASSWORD_RESET_SHOW_EMAIL_NOT_FOUND': True,
+    'EMAIL': {
+        'activation': 'djoser.email.ActivationEmail',
+    },
 }
 
 # Simple JWT settings
--- django_movie/urls.py
+++ django_movie/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from djoser.views import TokenCreateView
 from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
+                                            TokenRefreshView,)
 
 from users.views import UserListCreateAPIView, UserRetrieveUpdateDestroyAPIView
 
@@ -11,8 +12,9 @@ from users.views import UserListCreateAPIView, UserRetrieveUpdateDestroyAPIView
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api-auth/', include('rest_framework.urls')),
-    path('auth/users/', UserListCreateAPIView.as_view()),
+    path('auth/activate/<str:uid>/<str:token>/', TokenCreateView.as_view(), name='activate'),
     path('auth/users/me/', UserRetrieveUpdateDestroyAPIView.as_view()),
+#     path('auth/users/', UserListCreateAPIView.as_view()),
     path('auth/', include('djoser.urls')),
     path('auth/jwt/token/', TokenObtainPairView.as_view(),
          name='token_obtain_pair'),
